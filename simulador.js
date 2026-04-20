bad66ff5250fdd23879169eec7385baca5929a96
DavidGC28 <davicho9913@gmail.com>
2026-04-19 22:21:02 -0500

    recovery archivos

--- simulador.js
+++ simulador.js
@@ -4,12 +4,15 @@ const btnReiniciar = document.getElementById('btnReiniciar');
 btnCalcular.onclick = function() {
     
    
-    // Usamos Number() para convertir el texto en números operables
+    
     const ingresos = Number(document.getElementById('txtIngresos').value);
     const egresos = Number(document.getElementById('txtEgresos').value);
     const monto = Number(document.getElementById('txtMonto').value);
     const plazoAnios = Number(document.getElementById('txtPlazo').value);
     const tasa = Number(document.getElementById('txtTasaInteres').value);
 
-    // PASO B: Operaciones matemáticas
-    const disponible = ingresos - egresos;}
+   
+    const disponible = ingresos - egresos;
+
+}
+   
