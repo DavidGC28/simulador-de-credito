5ef0b31ada513e577d0ece0f61ea4cbcef62eb8f
DavidGC28 <davicho9913@gmail.com>
2026-04-19 22:40:59 -0500

    mod final

--- simulador.js
+++ simulador.js
@@ -11,8 +11,40 @@ btnCalcular.onclick = function() {
     const plazoAnios = Number(document.getElementById('txtPlazo').value);
     const tasa = Number(document.getElementById('txtTasaInteres').value);
 
-   
+    
     const disponible = ingresos - egresos;
+  
+    const capacidadPago = disponible * 0.4;
+
+    const interesTotal = monto * (tasa / 100) * plazoAnios;
+    const totalPrestamo = monto + interesTotal;
+    const cuotaMensual = totalPrestamo / (plazoAnios * 12);
+
+   
+    document.getElementById('spnDisponible').innerText = "$" + disponible.toLocaleString();
+    document.getElementById('spnCapacidadPago').innerText = "$" + capacidadPago.toLocaleString();
+    document.getElementById('spnInteresPagar').innerText = "$" + interesTotal.toLocaleString();
+    document.getElementById('spnTotalPrestamo').innerText = "$" + totalPrestamo.toLocaleString();
+    document.getElementById('spnCuotaMensual').innerText = "$" + cuotaMensual.toFixed(2);
+
+    
+    const estado = document.getElementById('spnEstadoCredito');
+    
+    if (cuotaMensual > capacidadPago) {
+        estado.innerText = "DENEGADO (Cuota supera capacidad)";
+        estado.style.color = "#ff4444"; 
+    } else if (monto <= 0) {
+        estado.innerText = "MONTO INVÁLIDO";
+        estado.style.color = "#ffbb33"; 
+    } else {
+        estado.innerText = "APROBADO ✅";
+        estado.style.color = "#00c851"; 
+};
+
+
+btnReiniciar.onclick = function() {
+   
+    location.reload();
+};
 
 }
-   
